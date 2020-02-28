92eab3c09f09fb405df4977709f9c4f52664c081
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2020-02-28 16:58:19 +0630

    adding pod file for firebase

---
+++ vocabList/Pods/Target Support Files/Pods-vocabList-vocabListUITests/Pods-vocabList-vocabListUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_vocabList_vocabListUITests : NSObject
+@end
+@implementation PodsDummy_Pods_vocabList_vocabListUITests
+@end
--- vocabList/vocabList/ViewController.swift
+++ vocabList/vocabList/ViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    var databaseRefer : DatabaseReference!
+    var databaseHandle : DatabaseHandle!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        databaseRefer = Database.database().reference()
+        
+//        let vocabData1 = ["cn" : "狗" as String,
+//                          "en" : "dog" as String
+//                        ]
+//        let vocabData2 = ["cn" : "好" as String,
+//                          "en" : "good"
+//        ]
+//        databaseRefer.child("vocabs").childByAutoId().setValue(vocabData1)
+//        databaseRefer.child("vocabs").childByAutoId().setValue(vocabData2)
+        
+        databaseRefer.child("vocabs").observeSingleEvent(of: .value, with: {snapshot in
+            if !snapshot.exists() {return}
+            print("Snapshot : \(snapshot)")
+            print("Snapshot value : \(snapshot.value)")
+            print("Snapshot key : \(snapshot.key)")
+            
+            for child in (snapshot.children){
+                print("child value : \(child)")
+                let vocabDataSnapShot = child as! DataSnapshot
+                let vocabDict = vocabDataSnapShot.value as! [String : Any]
+                let cnValue = vocabDict["cn"] as! String
+                let enValue = vocabDict["en"] as! String
+                
+                print("cn value : \(cnValue)")
+                print("en value : \(enValue)")
+                
+            }
+            
+            
+            
+        })
     }
 
 
