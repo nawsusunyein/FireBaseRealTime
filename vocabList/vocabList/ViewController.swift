376ff30f1feefb2707a3836e6c2005d9b2a2f137
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2020-02-28 16:56:29 +0630

    build project and insert method for retrieving data from firebase

---
+++ vocabList/vocabList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vocabList
+//
+//  Created by techfun on 2020/02/28.
+//  Copyright © 2020 Naw Su Su Nyein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
