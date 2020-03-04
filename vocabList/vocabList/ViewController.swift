9f8490e8bc011fbf105b04dea5ee69af456600cd
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2020-03-04 13:05:17 +0630

    insert vocab data

--- vocabList/vocabList/ViewController.swift
+++ vocabList/vocabList/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
     var databaseRefer : DatabaseReference!
     var databaseHandle : DatabaseHandle!
+    var vocabList : [VocabModel] = [VocabModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +31,12 @@ class ViewController: UIViewController {
 //        databaseRefer.child("vocabs").childByAutoId().setValue(vocabData2)
         
         databaseRefer.child("vocabs").observeSingleEvent(of: .value, with: {snapshot in
-            if !snapshot.exists() {return}
+            if !snapshot.exists() {
+                print("not exists")
+                self.insertVocabListData()
+                return
+                
+            }
             print("Snapshot : \(snapshot)")
             print("Snapshot value : \(snapshot.value)")
             print("Snapshot key : \(snapshot.key)")
@@ -42,6 +48,9 @@ class ViewController: UIViewController {
                 let cnValue = vocabDict["cn"] as! String
                 let enValue = vocabDict["en"] as! String
                 
+                let vocabData = VocabModel(cnValue: cnValue as NSString, enValue: enValue as NSString)
+                self.vocabList.append(vocabData)
+                
                 print("cn value : \(cnValue)")
                 print("en value : \(enValue)")
                 
@@ -52,6 +61,30 @@ class ViewController: UIViewController {
         })
     }
 
+    private func insertVocabListData(){
+        let vocabData1 = VocabModel(cnValue: "狗", enValue: "dog")
+        let vocabData2 = VocabModel(cnValue: "好", enValue: "good")
+        let vocabData3 = VocabModel(cnValue: "花", enValue: "flower")
+        let vocabData4 = VocabModel(cnValue: "玫瑰", enValue: "rose")
+        let vocabData5 = VocabModel(cnValue: "红", enValue: "red")
+        let vocabData6 = VocabModel(cnValue: "白", enValue: "white")
+        
+        vocabList.append(vocabData1)
+        vocabList.append(vocabData2)
+        vocabList.append(vocabData3)
+        vocabList.append(vocabData4)
+        vocabList.append(vocabData5)
+        vocabList.append(vocabData6)
+        
+        databaseRefer.child("vocabs").childByAutoId().setValue(vocabData1 as Any)
+        databaseRefer.child("vocabs").childByAutoId().setValue(vocabData2)
+        databaseRefer.child("vocabs").childByAutoId().setValue(vocabData3)
+        databaseRefer.child("vocabs").childByAutoId().setValue(vocabData4)
+        databaseRefer.child("vocabs").childByAutoId().setValue(vocabData5)
+        databaseRefer.child("vocabs").childByAutoId().setValue(vocabData6)
+        
+        print("vocab list count : \(vocabList.count)")
+    }
 
 }
 
---
+++ vocabList/vocabList/VocabModel.swift
@@ -0,0 +1,20 @@
+//
+//  VocabModel.swift
+//  vocabList
+//
+//  Created by techfun on 2020/02/28.
+//  Copyright © 2020 Naw Su Su Nyein. All rights reserved.
+//
+
+import Foundation
+
+class VocabModel {
+    
+    var cnValue : NSString?
+    var enValue : NSString?
+    
+    init(cnValue : NSString, enValue : NSString){
+        self.cnValue = cnValue
+        self.enValue = enValue
+    }
+}
